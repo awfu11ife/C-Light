2e8de4613334b3a7dba5354a2c074489de3af38b
awfu11ife <legj2013@yandex.ru>
2022-03-27 11:44:26 +0300

    HomeWork9

---
+++ HomeWork9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stopWord = "exit";
+            string usersWord = "";
+
+            while (stopWord != usersWord)
+            {
+                Console.WriteLine("Программа выполняеся......");
+                Console.WriteLine("Для продолжения нажмите Enter. Если вы вхотите завершить программу, введите 'exit'");
+                usersWord = Console.ReadLine();
+            }
+        }
+    }
+}
