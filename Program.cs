b06960c9762bc51c0bb561bc5ab46d333d2f5910
awfu11ife <legj2013@yandex.ru>
2022-03-27 11:18:09 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usersMessage;
+            int repetionsNumber;
+
+            Console.WriteLine("Введите своё сообщение");
+            usersMessage = Console.ReadLine();
+            Console.WriteLine("Сколько раз вы хотите его вывести на экран?");
+            repetionsNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < repetionsNumber; i++)
+            {
+                Console.WriteLine(usersMessage);
+            }
+        }
+    }
+}
