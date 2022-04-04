459685e838dd24861ddcc20bcfd3c436ff6ed42d
awfu11ife <legj2013@yandex.ru>
2022-04-04 23:11:14 +0300

    HomeWork27

--- Program.cs
+++ Program.cs
@@ -4,19 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeWork26
+namespace HomeWork27
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string sentence = "Практика с методом String.Split()";
-            string[] splitedSentence = sentence.Split();
+            int[] array = { 1, 2, 3, 4, 5 };
+            int shift;
 
-            foreach (var word in splitedSentence)
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine(word);
+                Console.Write(array[i] + " ");
             }
+
+            Console.WriteLine("\nНа сколько пунктов вы хотите сдвинуть массив?");
+            shift = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = shift; i > 0; i--)
+            {
+                int arrayFirstNumber = array[0];
+
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    array[j] = array[j + 1];
+                }
+
+                array[array.Length - 1] = arrayFirstNumber;
+            }
+
+            Console.WriteLine("Получится массив:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
