e828c32dc6071ecf2b5aaee53b39eab11703f8c3
awfu11ife <legj2013@yandex.ru>
2022-04-07 20:21:41 +0300

    HomeWork31

--- Program.cs
+++ Program.cs
@@ -4,38 +4,128 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeWork30
+namespace HomeWork31
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Здравствуйте!");
-            Console.WriteLine($"Число - {GetNumber()}");
+            char[,] map = { { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', },
+                            { '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', },
+                            { '#', ' ', ' ', ' ', '%', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', },
+                            { '#', ' ', ' ', '#', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', '#', },
+                            { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', },
+                            { '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', '#', ' ', ' ', '#', },
+                            { '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', 'X', '#', ' ', ' ', '#', },
+                            { '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', },
+                            { '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', },
+                            { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', }
+            };
+            char player = '%';
+            char finish = 'X';
+            int finishPositionX;
+            int finishPositionY;
+            int playerPositionX;
+            int playerPositionY;
+            bool isPlaying = true;
+
+            DrawMap(map, player, out playerPositionX, out playerPositionY, finish, out finishPositionX, out finishPositionY);
+            Move(map, player, finishPositionX, finishPositionY, ref playerPositionX, ref playerPositionY, ref isPlaying);
         }
 
-        static int GetNumber()
+        static void DrawMap(char[,] map, char player, out int playerPositionX, out int playerPositionY, char finish, out int finishPositionX, out int finishPositionY)
         {
-            int number = 0;
-            bool success = false;
+            playerPositionX = 0;
+            playerPositionY = 0;
+            finishPositionX = 0;
+            finishPositionY = 0;
 
-            while (success == false)
+            Console.CursorVisible = false;
+
+            for (int i = 0; i < map.GetLength(0); i++)
             {
-                Console.WriteLine("Введите число");
-                string uesrInput = Console.ReadLine();
-                success = int.TryParse(uesrInput, out number);
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
 
-                if (success == false)
-                {
-                    Console.WriteLine("К сожалению, это не число... Попробуйте ещё раз");
+                    if (map[i, j] == player)
+                    {
+                        playerPositionX = i;
+                        playerPositionY = j;
+                    }
+
+                    if (map[i, j] == finish)
+                    {
+                        finishPositionX = i;
+                        finishPositionY = j;
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Удивительно! У вас получилось ввести число!");
-                }
+
+                Console.WriteLine();
             }
+        }
 
-            return number;
+        static void Move(char[,] map, char player, int finishPositionX, int finishPositionY, ref int playerPositionX, ref int playerPositionY, ref bool isPlaying)
+        {
+
+            int playerDirectionX = 0;
+            int playerDirectionY = 0;
+
+            while (isPlaying == true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+
+                    switch (pressedKey.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            playerDirectionX = -1;
+                            playerDirectionY = 0;
+                            break;
+
+                        case ConsoleKey.DownArrow:
+                            playerDirectionX = 1;
+                            playerDirectionY = 0;
+                            break;
+
+                        case ConsoleKey.LeftArrow:
+                            playerDirectionX = 0;
+                            playerDirectionY = -1;
+                            break;
+
+                        case ConsoleKey.RightArrow:
+                            playerDirectionX = 0;
+                            playerDirectionY = 1;
+                            break;
+                    }
+
+                    if (map[playerPositionX + playerDirectionX, playerPositionY + playerDirectionY] != '#')
+                    {
+                        Console.SetCursorPosition(playerPositionY, playerPositionX);
+                        Console.Write(" ");
+                        playerPositionX += playerDirectionX;
+                        playerPositionY += playerDirectionY;
+                        Console.SetCursorPosition(playerPositionY, playerPositionX);
+                        Console.Write(player);
+                    }
+                }
+
+                Win(playerPositionX, playerPositionY, finishPositionX, finishPositionY, ref isPlaying);
+            }
+        }
+
+        static void Win(int playerPositionX, int playerPositionY, int finishPositionX, int finishPositionY, ref bool isPlaying)
+        {
+            int winCursorPositionX = 10;
+            int winCursorPositionY = 10;
+
+            if (playerPositionX == finishPositionX && playerPositionY == finishPositionY)
+            {
+                isPlaying = false;
+                Console.SetCursorPosition(winCursorPositionY, winCursorPositionX);
+                Console.WriteLine("WIN!!!!");
+            }
         }
     }
 }
