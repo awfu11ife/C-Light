b6fbcb68c4639551c5a96d953dcff9df83c2ebb4
awfu11ife <legj2013@yandex.ru>
2022-04-23 23:47:57 +0300

    HomeWork53

--- Program.cs
+++ Program.cs
@@ -4,55 +4,142 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeWork52
+namespace HomeWork53
 {
     class Program
     {
         static void Main(string[] args)
         {
-            IReadOnlyList<string> crimeTypes = new List<string> { "Антиправительственное", "Кражу", "Мошенничество", "Убийство" };
-            Prison prison = new Prison(crimeTypes, 10);
+            IReadOnlyList<string> diseases = new List<string> { "Простуда", "Ангина", "Грипп", "Пневмония" };
 
-            prison.ShowAllPrisoners();
-            prison.Amnesty();
-            prison.ShowAllPrisoners();
+            StartWork(diseases, 20);
+        }
+
+        static void StartWork(IReadOnlyList<string> deseases, int numberOfPatients)
+        {
+            const string ShowAll = "showall";
+            const string ShowWithSelectedDisease = "bydisease";
+            const string SortByAge = "byage";
+            const string SortByName = "byname";
+            const string ExitCommand = "exit";
+            string userInput = null;
+            DataBase dataBase = new DataBase(deseases, numberOfPatients);
+
+            Console.WriteLine("Это база данных с вашими пациентами");
+
+            while (userInput != ExitCommand)
+            {
+                Console.WriteLine($"Вам доступны следующие команды:\n" +
+                    $"{ShowAll} - показать всех пациентов\n" +
+                    $"{ShowWithSelectedDisease} - показать всех с выбранной болезнью\n" +
+                    $"{SortByAge} - отсортировать по возрасту\n" +
+                    $"{SortByName} - отсортировать по имени\n" +
+                    $"{ExitCommand} - выйти\n");
+                Console.WriteLine("Введите желаемую команду");
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case ShowAll:
+                        dataBase.ShowAll();
+                        break;
+
+                    case ShowWithSelectedDisease:
+                        dataBase.ShowWithSelectedDisease();
+                        break;
+
+                    case SortByAge:
+                        dataBase.SortByAge();
+                        break;
+
+                    case SortByName:
+                        dataBase.SortByName();
+                        break;
+
+                    case ExitCommand:
+                        break;
+
+                    default:
+                        Console.WriteLine("Такой команды нет...\n");
+                        break;
+                }
+            }
         }
     }
 
-    class Prison
+    class DataBase
     {
-        private List<Prisoner> _prisoners = new List<Prisoner>();
-        private List<string> _crimeTypes = new List<string>();
+        private List<Patient> _allPatients = new List<Patient>();
+        private List<string> _diseases = new List<string>();
 
-        public Prison(IReadOnlyList<string> crimeTypes, int number)
+        public DataBase(IReadOnlyList<string> diseases, int numberOfPatients)
         {
-            _crimeTypes = (List<string>)crimeTypes;
-            Create(number);
+            _diseases = (List<string>)diseases;
+            Create(numberOfPatients);
         }
 
-        public void ShowAllPrisoners()
+        public void ShowAll()
         {
-            Console.WriteLine("В тюрьме сейчас сидят:\n");
+            Console.WriteLine("Сейчас вы лечите следующих пациентов:\n");
 
-            foreach (var prisoner in _prisoners)
+            foreach (var patient in _allPatients)
             {
-                prisoner.ShowInfo();
+                patient.ShowInfo();
             }
+
+            Console.ReadKey();
+            Console.Clear();
         }
 
-        public void Amnesty()
+        public void ShowWithSelectedDisease()
         {
-            string amnestyCrimeType = "Антиправительственное";
+            Console.WriteLine("Введите название болезни");
+            string userInput = Console.ReadLine();
 
-            if (_crimeTypes.Contains(amnestyCrimeType))
+            if (_diseases.Contains(userInput))
             {
-                _prisoners = _prisoners.Where(prisoner => prisoner.CrimeType != amnestyCrimeType).ToList();
-                Console.WriteLine("\nПроизошла амнистия\n");
+                var sortedPatients = _allPatients.Where(patient => patient.Disease == userInput);
+
+                foreach (var patient in sortedPatients)
+                {
+                    patient.ShowInfo();
+                }
             }
             else
             {
-                Console.WriteLine("Такого приступления нет");
+                Console.WriteLine("Такой болезни нет");
             }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void SortByAge()
+        {
+            var sortedPatients = _allPatients.OrderBy(player => player.Age);
+            Console.WriteLine("Список, отсортированный по возрасту:\n");
+
+            foreach (var patient in sortedPatients)
+            {
+                patient.ShowInfo();
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void SortByName()
+        {
+            var sortedPatients = _allPatients.OrderBy(player => player.Name);
+            Console.WriteLine("Список, отсортированный по именам:\n");
+
+            foreach (var patient in sortedPatients)
+            {
+                patient.ShowInfo();
+            }
+
+            Console.ReadKey();
+            Console.Clear();
         }
 
         private void Create(int number)
@@ -61,27 +148,31 @@ namespace HomeWork52
 
             for (int i = 0; i < number; i++)
             {
-                _prisoners.Add(new Prisoner(_crimeTypes, random));
+                _allPatients.Add(new Patient(_diseases, random));
             }
         }
     }
 
-    class Prisoner
+    class Patient
     {
         public string Name { get; private set; }
-        public string CrimeType { get; private set; }
+        public string Disease { get; private set; }
+        public int Age { get; private set; }
 
-        public Prisoner(IReadOnlyList<string> crimeTypes, Random random)
+        public Patient(IReadOnlyList<string> diseases, Random random)
         {
             List<string> names = new List<string> { "Max", "John", "George", "Leon", "Boris", "David" };
+            int maxAge = 70;
+            int minAge = 18;
 
             Name = names[random.Next(0, names.Count)];
-            CrimeType = crimeTypes[random.Next(0, crimeTypes.Count)];
+            Disease = diseases[random.Next(0, diseases.Count)];
+            Age = random.Next(minAge, maxAge);
         }
 
         public void ShowInfo()
         {
-            Console.WriteLine($"Заключенный по имени {Name} осужден за {CrimeType}");
+            Console.WriteLine($"Пациент по имени {Name}, полных лет - {Age}, заболевание - {Disease}");
         }
     }
 }
